7895833fd30da5459e3e505f7b13bbb400762d48
Paul Bouquet <paul.bouquet@gmail.com>
2016-11-15 17:55:03 +0100

    debut du codage de gps

--- gps.h
+++ gps.h
@@ -9,4 +9,21 @@
 #ifndef __ROBOT_GPS_HEADER
 #define __ROBOT_GPS_HEADER
 
+// Constantes de circuits
+int C1[] 	= {};
+int C2[] 	= {};
+int C3[] 	= {};
+int C4[] 	= {};
+int C5[] 	= {};
+int C6[] 	= {};
+int C7[] 	= {};
+int C8[] 	= {};
+int C9[] 	= {};
+int C10[] 	= {};
+int C11[] 	= {};
+int C12[] 	= {};
+
+// Protoypes
+void start_circuit(void);
+
 #endif
